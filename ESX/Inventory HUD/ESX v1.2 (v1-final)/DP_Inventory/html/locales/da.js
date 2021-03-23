e39bfc70097263819cedb37017aaf6b62ef441d7
sorenmarquardt <soren8620@hotmail.com>
2021-03-23 12:53:10 +0100

    Dansk oversættelse

---
+++ ESX/Inventory HUD/ESX v1.2 (v1-final)/DP_Inventory/html/locales/da.js
@@ -0,0 +1,5 @@
+var invLocale = new Object();
+invLocale.dropItem = "Smid";
+invLocale.useItem = "Brug";
+invLocale.giveItem = "Giv";
+invLocale.secondInventoryNotAvailable = "Andet inventory er ikke tilgængelig.";
